a91bdcadefa4d6b0c01595544b63b660ea983c8a
Jason Grace <jason@thegracefamily.net>
2017-06-17 22:46:05 -0400

    Package setup

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// Include Gulp
+var gulp = require('gulp');
+
+var yarn = require('gulp-yarn');
+
+gulp.task('yarn', function() {
+    return gulp.src(['./package.json', './yarn.lock'])
+        .pipe(gulp.dest('./dist'))
+        .pipe(yarn({
+            production: true
+        }));
+});
+
+gulp.task('clean', [], function() {
+    console.log("Clean all files in build folder");
+
+    return gulp.src("build/*", { read: false }).pipe(clean());
+});
+
+gulp.task('js', function() {
+    return gulp.src('js/*.js')
+        .pipe(gulp.dest('build'));
+});
+
+// Default Task
+gulp.task('default', ['yarn', 'js']);
