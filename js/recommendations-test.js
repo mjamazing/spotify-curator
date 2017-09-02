01b6a71cfb14c7a6d76afae1890f8e4a648e82a0
Jason Grace <jason@thegracefamily.net>
2017-09-01 22:02:46 -0400

    Working recommendation test

--- js/recommendations-test.js
+++ js/recommendations-test.js
@@ -9,26 +9,15 @@ console.log("In the index");
 
 spotifyApi.clientCredentialsGrant()
     .then(function(data) {
-            console.log('The access token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-
             // Save the access token so that it's used in future calls
             spotifyApi.setAccessToken(data.body['access_token']);
 
-            spotifyApi.getAudioFeaturesForTrack('3Qm86XLflmIXVm1wcwkgDK')
+            spotifyApi.getRecommendations({ min_energy: 0.7, min_danceability: 0.5, min_energy: 0.7, seed_tracks: ['1NZWiuy0mlnsrcYL2dhKt6'], limit: 100, max_liveness: 0.1, target_popularity: 50 })
                 .then(function(data) {
                     console.log(data.body);
                 }, function(err) {
                     console.log(err);
                 });
-
-            spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-                .then(function(data) {
-                    console.log('Albums information', data.body);
-                }, function(err) {
-                    console.error(err);
-                });
-
         },
         function(err) {
             console.log('Something went wrong when retrieving an access token', err.message);
